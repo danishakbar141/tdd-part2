c78389e7e0a925e0df57810e2609d53a52b41601
Danish Akbar <daniak141@yahoo.com>
2015-01-02 12:23:55 +0500

    Example 1 of 18
    Example 1 of 18

---
+++ WasRun.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package part2;
+
+/**
+ *
+ * @author Danish Akbar
+ */
+public class WasRun {
+    public boolean wasRun;
+
+    public WasRun() {
+        this.wasRun = false;
+    }
+    public void run(){
+        this.wasRun=true;
+    }
+    
+}
