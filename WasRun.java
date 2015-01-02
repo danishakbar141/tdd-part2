7913194dbe0a160bbb6906269f6f83fa2434f7a2
Danish Akbar <daniak141@yahoo.com>
2015-01-03 01:21:32 +0500

    example 20 part 2

--- WasRun.java
+++ WasRun.java
@@ -10,22 +10,24 @@ package part2;
  */
 public class WasRun extends TestCase{
     public boolean wasRun;
-    public boolean wasSetup;
     public String log;
     
 
     public WasRun(String name) {
         super(name);
-        this.wasRun = false;
+        wasSetup();
     }
     private void testMethod(){
         wasRun=true;
+        log+="testMethod";
     }
 
    public void wasSetup() {
         wasRun=false;
-        wasSetup=true;
-        log="wasSetup";
+        log="wasSetup ";
     }
+   public void tearDown(){
+   log+="tearDown";
+   }
     
 }
